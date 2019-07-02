d251fd0e25fa56c73181835f2b5f636ff04c76ab
Tom Woodward <twwoodward@me.com>
2019-07-02 09:15:25 -0400

    fix youtube overflow size

--- js/plus-editor.js
+++ js/plus-editor.js
@@ -34,3 +34,15 @@ jQuery(document).ready(function(){
         console.log('fired it')
     }
 });
+
+//full size youtube video stuff
+
+var videos = document.querySelectorAll('iframe[src^="https://www.youtube.com/"], iframe[src^="https://player.vimeo.com"], iframe[src^="https://www.youtube-nocookie.com/"], iframe[src^="https://www.nytimes.com/"]'); //get video iframes for regular youtube, privacy+ youtube, and vimeo
+
+
+videos.forEach(function(video) {
+   let wrapper = document.createElement('div'); //create wrapper 
+      wrapper.classList.add("video-responsive"); //give wrapper the class      
+      video.parentNode.insertBefore(wrapper, video); //insert wrapper      
+      wrapper.appendChild(video); // move video into wrapper
+});
